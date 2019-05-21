728c641069e18986bf06845a06e7bee0a72da20d
bratzaharia <bratzaharia@gmail.com>
2019-05-21 12:23:14 +0300

    First commit

---
+++ src/components/layout.js
@@ -0,0 +1,28 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's StaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/static-query/
+ */
+
+import React from "react"
+import PropTypes from "prop-types"
+import { StaticQuery, graphql } from "gatsby"
+
+import "./bootstrap.min.css"
+import "./layout.css"
+
+import NavBar from "./GLobals/NavBar"
+
+const Layout = ({ children }) => (
+  <>
+    <NavBar />
+    {children}
+  </>
+)
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
